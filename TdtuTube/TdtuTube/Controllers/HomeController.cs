23a1d21d8c85f337a129993a9e4bddb5cbc362c9
anhuynh02 <105099290+anhuynh02@users.noreply.github.com>
2023-02-14 16:27:35 +0700

    Load Tags in homepage

--- TdtuTube/TdtuTube/Controllers/HomeController.cs
+++ TdtuTube/TdtuTube/Controllers/HomeController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using TdtuTube.Models;
 namespace TdtuTube.Controllers
 {
     public class HomeController : Controller
     {
+        TdtuTubeEntities db = new TdtuTubeEntities();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +26,12 @@ namespace TdtuTube.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult getMenu() {
+            var v = from t in db.Tags
+                    orderby t.id ascending
+                    select t;
+            return PartialView(v.ToList());
         }
     }
 }
