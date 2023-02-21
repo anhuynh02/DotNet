d919938ff8f07dc411a8e79c6a18a84e7a4bf06d
anhuynh02 <105099290+anhuynh02@users.noreply.github.com>
2023-02-21 19:46:09 +0700

    update js function

--- TdtuTube/TdtuTube/Content/JS/script.js
+++ TdtuTube/TdtuTube/Content/JS/script.js
@@ -1,6 +1,24 @@
 let videosContainer = document.getElementById('videos-container');
 let videoCard = "";
-
+let mainContainer = document.getElementById("videoMainContainer");
+let menuBtn = document.getElementById('menu');
+menuBtn.addEventListener("click", display_sidebar);
+function display_sidebar() {
+    isShow = document.getElementById("sideBar");
+    if (isShow.style.display === "none") {
+        isShow.style.display = "block"
+        mainContainer.classList.add("lg:ml-60");
+        mainContainer.classList.add("lg:w-4/5");
+        mainContainer.classList.add("xl:w-5/6");
+        mainContainer.classList.remove("lg:w-5/5")
+    } else {
+        isShow.style.display = "none"
+        mainContainer.classList.remove("lg:ml-60");
+        mainContainer.classList.remove("lg:w-4/5");
+        mainContainer.classList.remove("xl:w-5/6");
+        mainContainer.classList.add("lg:w-5/5")
+    }
+}
 fetch("assets/videos.json")
 .then(response => response.json())
 .then(videos => {
