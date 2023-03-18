351928da336eacede0f3b7fb4e7aeadc1f83fada
anhuynh02 <105099290+anhuynh02@users.noreply.github.com>
2023-03-18 21:57:27 +0700

    Update search FE + css

--- TdtuTube/TdtuTube/Controllers/SearchController.cs
+++ TdtuTube/TdtuTube/Controllers/SearchController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TdtuTube.Models;
 
 namespace TdtuTube.Controllers
 {
+
     public class SearchController : Controller
     {
+        private TdtuTubeEntities db = new TdtuTubeEntities();
         // GET: Search
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult searchVideos(string meta)
+        {
+            var v = from i in db.Videos
+                    where i.title.Contains(meta) && i.privacy == false && i.hide == false && i.status == false
+                    orderby i.order ascending
+                    select i;
+            return PartialView(v.ToList());
         }
     }
 }
