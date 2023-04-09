8ce38cf6d1ea118721164e701e476f7f66bfa200
anhuynh02 <105099290+anhuynh02@users.noreply.github.com>
2023-04-09 15:26:38 +0700

    add signup

--- TdtuTube/TdtuTube/Controllers/LoginController.cs
+++ TdtuTube/TdtuTube/Controllers/LoginController.cs
@@ -47,6 +47,56 @@ namespace TdtuTube.Controllers
             
             return Redirect("/login");
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PostSignUp(string email, string password, string name)
+        {
+            if (ModelState.IsValid)
+            {
+                var v = from i in db.Users
+                        where i.email == email
+                        select i;
+                var user = v.FirstOrDefault();
+                if (user == null)
+                {
+
+                    User user1 = new User();
+                    user1.email = email;
+                    user1.name = name;
+                    user1.password = BC.HashPassword(password);
+                    user1.avatar_path = "/Uploads/Avatars/default.PNG";
+                    user1.meta = "@" + user1.id;
+                    user1.role_id = 1;
+                    user1.hide = false;
+                    user1.order = 0;
+                    user1.subscriber_count = 0;
+                    user1.datebegin = DateTime.Now;
+                    db.Entry(user1).State = System.Data.Entity.EntityState.Added;
+                    db.SaveChanges();
+
+                    var p = from i in db.Users
+                            where i.email == email
+                            select i;
+                    var user2 = p.FirstOrDefault();
+                        Session["UserID"] = user2.id;
+                        Session["UserRoleID"] = user2.role_id;
+                        Session["UserName"] = user2.name;
+                        Session["UserAvatarPath"] = user2.avatar_path;
+                        Session["UserMeta"] = user2.meta;
+                        Session["Error"] = null;
+                        if (Session["ReturnURL"] != null)
+                        {
+                            string URL = (string)Session["ReturnURL"];
+                            return Redirect(URL);
+                        }
+
+               
+                }
+            }
+
+            return Redirect("/login");
+        }
         public ActionResult logout()
         {   
             //Delete the Session object.
