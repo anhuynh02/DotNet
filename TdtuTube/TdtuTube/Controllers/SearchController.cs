7c1afcdb9a5c3e8145b3e0acd63184e3d9e65a04
anhuynh02 <105099290+anhuynh02@users.noreply.github.com>
2023-05-12 19:16:15 +0700

    update search feature

--- TdtuTube/TdtuTube/Controllers/SearchController.cs
+++ TdtuTube/TdtuTube/Controllers/SearchController.cs
@@ -31,5 +31,34 @@ namespace TdtuTube.Controllers
             var v = t.ToList().Where(i => VideoFormat.normalize(i.title).Contains(searchQuery.ToLower()));
             return PartialView(v);
         }
+        public ActionResult searchChannel(string searchQuery)
+        {
+            if (searchQuery == null)
+            {
+                return PartialView(new List<Video>());
+            }
+            var t = from i in db.Users
+                    where i.hide == false && i.status == false
+                    orderby i.order ascending
+                    select i;
+            ".".Contains(".");
+            var v = t.ToList().Where(i => VideoFormat.normalize(i.name).Contains(searchQuery.ToLower()));
+            return PartialView(v.Take(5));
+        }
+
+        public ActionResult searchVideosWithUser(string searchQuery)
+        {
+            if (searchQuery == null)
+            {
+                return PartialView(new List<Video>());
+            }
+            var t = from i in db.Videos
+                    where i.privacy == false && i.hide == false && i.status == false
+                    orderby i.order ascending
+                    select i;
+            ".".Contains(".");
+            var v = t.ToList().Where(i => VideoFormat.normalize(i.User.name).Contains(searchQuery.ToLower()));
+            return PartialView(v);
+        }
     }
 }
